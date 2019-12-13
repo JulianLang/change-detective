4837bdb9e4d8e35adea3b3389e99d43e473d2e9f
Julian Lang <julian.lang@hotmail.de>
2019-12-14 00:04:53 +0100

    fix: typings of subscribe callback

--- src/types/subscribe-callback.ts
+++ src/types/subscribe-callback.ts
@@ -1,6 +1,7 @@
+import { Member } from './member';
 import { PropertyChange } from './property.change';
 
 /** Callback function to be called whenever a change occurs. */
 export interface SubscribeCallback<T = any> {
-  (change: PropertyChange<T>): void;
+  (change: PropertyChange<Member<T>>): void;
 }
