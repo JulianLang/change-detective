f4fa7ae28c0da95b1f642686ed744ad4f78a29b6
Julian Lang <julian.lang@hotmail.de>
2019-12-19 22:17:59 +0100

    chore: playground

--- playground.ts
+++ playground.ts
@@ -1,16 +1,25 @@
 import { detectChanges } from './src/detect-changes';
 import { ChangeDetective } from './src/types';
 
-const numberArr: number[] = [];
-const _value = detectChanges(numberArr);
+const _value = detectChanges(
+  {
+    a: {
+      b: 42,
+      c: true,
+    },
+  },
+  { strictComparison: false },
+);
 
 const unsubscribe = _value[ChangeDetective].subscribe(change => {
   console.log(change);
 });
 
-_value.push(1);
-_value.pop();
+_value.a = {
+  b: 42,
+  c: true,
+};
 
 unsubscribe();
-console.log(_value[ChangeDetective].changes);
+console.log(_value[ChangeDetective].changes());
 console.log();
